cc1ff91f67c949bd60e8962c7c84ceed942e2553
Illia Brytun <illiabrytun@gmail.com>
2020-04-18 21:26:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function breakOut(array, changeValue, stopValue) {
     array[i] = changeValue
   }
 }
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i=0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i] = changeValue
+  }
+  return array
+}
+
